c4e3e0779e16c7657d7a62f831869011beb84c4a
roenc576 <roenc576@gmail.com>
2024-05-07 19:15:05 -0400

    Seeded users table

--- server/db/seeds/init.js
+++ server/db/seeds/init.js
@@ -1,4 +1,3 @@
-const User = require('../models/User');
 /**
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
@@ -7,9 +6,25 @@ exports.seed = async (knex) => {
   // Before you have models you can always just do `await knex('table_name').del`
   await knex('users').del();
 
-  await knex.raw('ALTER SEQUENCE users_id_seq RESTART WITH 1');
 
-  await User.create('cool_cat', '1234');
-  await User.create('l33t-guy', '1234');
-  await User.create('wowow', '1234');
+  await knex('users').insert([
+    {
+      full_name: 'Linden Salm',
+      username: 'floraleopard',
+      password_hash: '473892',
+      email: 'floraleopard@gmail.com',
+    },
+    {
+      full_name: 'Mohamad Eastwood',
+      username: 'chaospider',
+      password_hash: '58028289',
+      email: 'chaospider@gmail.com',
+    },
+    {
+      full_name: 'Hana Kwok',
+      username: 'wheatglacier',
+      password_hash: '8908930',
+      email: 'wheatglacier@gmail.com',
+    },
+  ]);
 };
