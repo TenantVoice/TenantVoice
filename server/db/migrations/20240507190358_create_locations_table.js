d29ed011bad3904df264c2a24f3a7cebb9ebae1f
roenc576 <roenc576@gmail.com>
2024-05-07 15:51:34 -0400

    Created migration for locations table

---
+++ server/db/migrations/20240507190358_create_locations_table.js
@@ -0,0 +1,19 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = (knex) => {
+    return knex.schema.createTable('locations', (table) => {
+        table.increments();
+        table.string('borough').notNullable();
+        table.string('neighborhood').notNullable();
+        table.string('complex').notNullable();
+        table.integer('building').notNullable();
+    })
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = (knex) => knex.schema.dropTable('locations');
