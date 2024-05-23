872ba05a7785fc68e04cdcdbf6f7a554cc787cfd
nico-j3 <nicolejuarezflores@gmail.com>
2024-05-22 23:20:15 -0400

    temp

--- frontend/src/pages/Posts/Posts.jsx
+++ frontend/src/pages/Posts/Posts.jsx
@@ -4,7 +4,7 @@ import FlyoutNav from "../../components/FlyoutNav";
 import ReportForm from "../../components/ReportForm"
 import { ChakraProvider } from '@chakra-ui/react'
 import NewPostCard from "../../components/PostCards";
-
+import "./postsPg.css"
 
 export default function PostPage() {
   const [posts, setPosts] = useState([]);
@@ -14,27 +14,31 @@ export default function PostPage() {
   }, []);
   console.log(posts);
 
-  return <>
+  return (
 
-    <FlyoutNav />
-    <div className="flex pl-10 mt-[60px]">
-      <div className="pr-10 z-1000">
-        <ReportForm setPosts={setPosts} />
+    <>
+      <FlyoutNav />
+      <div className="bg-oxford">
+        <div className="bg-oxford flex pl-10 mt-[60px]">
+          <div className="pr-10 z-1000 bg-oxford">
+            <ReportForm setPosts={setPosts} />
+          </div>
+          <div className="absolute top-[50%] left-[50%]">
+            <ul className="posts-list">
+              {posts?.map((post) => {
+                return (
+                  <li key={post.id}>
+                    <ChakraProvider>
+                      <NewPostCard post={post} />
+                    </ChakraProvider>
+                  </li>
+                );
+              })}
+            </ul>
+          </div>
+        </div>
       </div>
-      <div className="absolute top-[50%] left-[50%]">
-        <ul className="posts-list">
-          {
-            posts?.map((post) => {
-              return <li key={post.id}>
-                <ChakraProvider>
-                  <NewPostCard post={post} />
-                </ChakraProvider>
-              </li>
-            })
-          }
-        </ul>
-      </div>
-    </div >
-  </>;
+    </>
+  );
 }
 
